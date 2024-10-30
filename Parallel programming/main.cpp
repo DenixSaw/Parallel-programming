0070240438fb9f70d2298dffc071471a7e8a5fe5
rochaldo1 <xusainow.artur2018@yandex.ru>
2024-10-30 13:06:47 +0700

    merge with txt

--- Parallel programming/main.cpp
+++ Parallel programming/main.cpp
@@ -1,13 +1,30 @@
 ﻿#include <iostream>
 #include <thread>
 #include <locale>
-
+#include "thread_data.h"
 using namespace std;
 
+
+int calc_gcd(int a, int b) {
+	while (a != b) {
+		if (a > b) {
+			a -= b;
+		}
+		else {
+			b -= a;
+		}
+	}
+	return b;
+}
+
+void run_thread(*thread_data data) {
+
+}
 
 int main()
 {
 	setlocale(0, "RUS");
+	
 
 
 	return EXIT_SUCCESS;
